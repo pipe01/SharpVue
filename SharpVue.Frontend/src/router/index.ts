1b4f41ee257fbad70141ed14a9555c33d1137048
Felipe Martinez <pipernene@gmail.com>
2020-11-07 22:28:42 +0100

    Add home page

--- SharpVue.Frontend/src/router/index.ts
+++ SharpVue.Frontend/src/router/index.ts
@@ -1,13 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 import ViewReference from "@/views/ViewReference.vue";
+import Home from "@/views/Home.vue";
 
 const routes = [
-    {
-        path: '/ref/:item?/:member?',
-        name: 'ViewReference',
-        component: ViewReference
-    }
+    { path: "/", name: "Home", component: Home },
+    { path: '/ref/:item?/:member?', name: 'ViewReference', component: ViewReference }
 ]
 
 const router = createRouter({
---
+++ SharpVue.Frontend/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+.container
+    h1 {{appName}} documentation
+
+    p
+        | This is the main page for the {{appName}} documentation, you can go to #[router-link(:to="{name:'ViewReference'}") the API reference page] to browse the API.
+</template>
+
+
+<script lang="ts" setup>
+import { inject } from 'vue';
+
+export const appName = inject("appName");
+</script>
